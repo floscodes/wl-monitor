a92092acbe93cfd6ad2bd73157c20cfd17500f6b
flopetautschnig <fp-others@mailbox.org>
2026-01-08 19:04:41 +0100

    fix: Service worker

--- src/pwa/service_worker.rs
+++ src/pwa/service_worker.rs
@@ -4,11 +4,10 @@ const SW_JS: Asset = asset!("/assets/js/android_service_worker.js");
 
 #[inline]
 pub async fn run() {
-    let sw_path = SW_JS.to_string();
     let js_code = format!(r#"
         if ('serviceWorker' in navigator) {{
                 window.addEventListener('load', () => {{
-                    navigator.serviceWorker.register("{sw_path}");
+                    navigator.serviceWorker.register("{SW_JS}");
             }});
         }}
     "#);
