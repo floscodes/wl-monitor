f642ca2005a16c597641c42cbc9410dc684b6ae4
flopetautschnig (floscodes) <mail@floscodes.net>
2026-01-08 16:41:07 +0100

    fix: manifest

--- src/pwa/manifest_json.rs
+++ src/pwa/manifest_json.rs
@@ -4,8 +4,6 @@ use urlencoding;
 
 pub fn generate_manifest_href(icon_192: Asset, icon_512: Asset) -> String {
     let base_url = "https://floscodes.github.io/wl-monitor/";
-    let icon_192 = icon_192.to_string();
-    let icon_512 = icon_512.to_string();
     let icon_192 = format!("{base_url}{icon_192}{icon_192}");
     let icon_512 = format!("{base_url}{icon_512}{icon_512}");
     let manifest_json = format!(r##"
